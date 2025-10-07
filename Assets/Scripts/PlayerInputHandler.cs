e50bbb5c605d463dee3b7b14486e394761a48826
Matthew Dadoun <33492474+matthewdadoun@users.noreply.github.com>
2025-10-06 23:12:16 -0400

    Update PlayerInputHandler.cs
    Player Input Handler - add horizontal movement / acceleration

--- Assets/Scripts/PlayerInputHandler.cs
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -1,23 +1,46 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerInputHandler : MonoBehaviour
 {
     // This is the default input system asset.
     private PlayerInputActions _controls;
     
+    // The rigid body of the character
+    private Rigidbody _rb;
     
+    // The move input used to determine how the player moves.
     private Vector2 _moveInput;
+    
+    // Whether the player is jumping or not.
     private bool _isJumping;
+    
+    // Whether the player is throwing or not.
     private bool _isThrowing;
+    
+    // Movement tuning variables
+    [Header("Movement Tuning")]
+
+    // The speed at which the player moves on the ground
+    public float moveSpeed = 8f;
+    // The acceleration of the player when moving
+    public float accel = 60f;
+    // The acceleration of the player when jumping
+    public float airAccel = 10f;
 
     void Awake()
     {
+        
+        // Create new player input actions object
         _controls = new PlayerInputActions();
+        
+        // Get the rigid body of the character
+        _rb = GetComponent<Rigidbody>();
 
+        // Handlers for when move is performed
         _controls.Player.Move.performed += ctx => _moveInput = ctx.ReadValue<Vector2>();
         _controls.Player.Move.canceled += ctx => _moveInput = Vector2.zero;
 
+        // Jump and throw handlers
         _controls.Player.Jump.performed += ctx => _isJumping = true;
         _controls.Player.PickUpThrow.performed += ctx => _isThrowing = true;
     }
@@ -32,10 +55,11 @@ public class PlayerInputHandler : MonoBehaviour
         _controls.Player.Disable();
     }
 
-    void Update()
+    /*void Update()
     {
         // Pass this info to your movement / throw logic
         Debug.Log("Move: " + _moveInput);
+        
         if (_isJumping)
         {
             Debug.Log("Jump!");
@@ -46,6 +70,22 @@ public class PlayerInputHandler : MonoBehaviour
             Debug.Log("Throw!");
             _isThrowing = false;
         }
+    }
+    */
+
+    private void FixedUpdate()
+    {
+        // Retrieve velocity
+        var currentVel = _rb.linearVelocity;
+
+        // Store velocity X
+        var targetVelX = _moveInput.x * moveSpeed;
+        
+        // Accelerate velocity using acceleration
+        currentVel.x = Mathf.MoveTowards(currentVel.x, targetVelX, accel * Time.fixedDeltaTime);
+
+        // Store current velocity
+        _rb.linearVelocity = currentVel;
     }
 
     public Vector2 GetMoveInput() => _moveInput;
