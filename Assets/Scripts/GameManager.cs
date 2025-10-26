7a3e55637b1f97b828d7186434e5931db6df8e5a
Matthew Dadoun <33492474+matthewdadoun@users.noreply.github.com>
2025-10-26 16:36:16 -0400

    Add new Game Manager object (+ as prefab) to PersistentPlayerScene + new script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+[DefaultExecutionOrder(-1000)] // make sure it initializes early
+public class GameManager : MonoBehaviour
+{
+    // The single instance of this game manager
+    private static GameManager Instance { get; set; }
+    
+    // Retrieve fade controller instance
+    public FadeController FadeControllerInstance { get; private set; }
+
+    private void Awake()
+    {
+        // Check if there is an old existing instance
+        if (Instance != null && Instance != this)
+        {
+            // Destroy the old lingering game object if the instance is not null
+            Destroy(gameObject); 
+            return;
+        }
+        
+        // Store instance / don't destroy on load
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        // Grab the UI even if it's inactive
+        FadeControllerInstance = FindAnyObjectByType<FadeController>();
+    }
+}
