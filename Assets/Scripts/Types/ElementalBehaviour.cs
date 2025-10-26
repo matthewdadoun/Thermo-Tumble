80c6c32e9ecdcb0a521b9d729aaaaa7642e83a98
Matthew Dadoun <33492474+matthewdadoun@users.noreply.github.com>
2025-10-25 23:08:18 -0400

    Update ElementalBehavior.cs to support any type of renderer updating materials

--- Assets/Scripts/Types/ElementalBehaviour.cs
+++ Assets/Scripts/Types/ElementalBehaviour.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -18,66 +17,71 @@ public abstract class ElementalBehaviour : MonoBehaviour
     [SerializeField] protected ElementType element;
     private ElementalMaterials _elementMats;
 
-    private SkinnedMeshRenderer _mesh;
+    private Renderer _renderer;
 
     private void Awake()
     {
         // Store mesh renderer
-        _mesh = gameObject.GetComponent<SkinnedMeshRenderer>();
+        _renderer = gameObject.GetComponent<Renderer>();
 
         if (_elementMats == null)
         {
             // retrieve elemental materials singleton
             _elementMats = ElementalMaterials.Instance;
         }
+
+        // Set element's material
+        SetElement(element);
     }
 
     // Getter / setter for element type
     public ElementType Element
     {
         get => element;
-        set
+    }
+
+    public void SetElement(ElementType elementType)
+    {
+        // Update element type
+        
+        element = elementType;
+        if (!_elementMats)
         {
-            element = value;
-
-            if (!_elementMats)
-            {
-                return;
-            }
-
-            Material mat = null;
-            foreach (var elementMat in _elementMats.elementMaterials)
-            {
-                // if we found the matching element
-                if (elementMat.element == element)
-                {
-                    // store element material
-                    mat = elementMat.material;
-                }
-            }
-
-            // if material is null
-            if (!mat)
-            {
-                return;
-            }
-            
-            // Set the material to ice
-            List<Material> materials = new List<Material>(_mesh.materials);
-            for (int i = 0; i < materials.Count; i++)
-            {
-                materials[i] = mat;
-            }
-
-            // set new materials
-            _mesh.SetMaterials(materials);
+            return;
         }
+
+        Material mat = null;
+        foreach (var elementMat in _elementMats.elementMaterials)
+        {
+            // if we found the matching element
+            if (elementMat.element == elementType)
+            {
+                // store element material
+                mat = elementMat.material;
+            }
+        }
+
+        // if material is null
+        if (!mat)
+        {
+            return;
+        }
+            
+        // Set the material to ice
+        List<Material> materials = new List<Material>(_renderer.materials);
+        for (int i = 0; i < materials.Count; i++)
+        {
+            materials[i] = mat;
+        }
+
+        // set new materials
+        _renderer.SetMaterials(materials);
     }
 
     public virtual void ReactTo(ElementType other)
     {
-        /*// Shared default behavior
-        var reaction = ElementReactions.Get(element, other);
+        // Shared default behavior
+        /*var reaction = ElementReactions.Get(element, other);
         Debug.Log($"{name} ({element}) reacts to {other}: {reaction}");*/
     }
 }
