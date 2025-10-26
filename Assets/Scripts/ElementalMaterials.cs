e3e9bcdedd87a48261fa189d4f45788d802ced59
Matthew Dadoun <33492474+matthewdadoun@users.noreply.github.com>
2025-10-25 22:46:15 -0400

    ElementalMaterials - new static function to retrieve single instance of materials asset

--- Assets/Scripts/ElementalMaterials.cs
+++ Assets/Scripts/ElementalMaterials.cs
@@ -9,9 +9,27 @@ public struct ElementMaterialPair
     public Material material;
 }
 
-[CreateAssetMenu(fileName = "ElementalMaterials", menuName = "Scriptable Objects/ElementalMaterials")]
+[CreateAssetMenu(fileName = "ElementalMaterialsDB", menuName = "Scriptable Objects/ElementalMaterials")]
 public class ElementalMaterials : ScriptableObject
 {
     // Dictionary lookup used to find material
     [SerializeField] public ElementMaterialPair[] elementMaterials;
+
+
+    // Simple singleton-style access with lazy load
+    public const string ResourcePath = "ElementalMaterialsDB"; // or "Configs/ElementalMaterialsDB"
+    private static ElementalMaterials _instance;
+
+    public static ElementalMaterials Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = Resources.Load<ElementalMaterials>(ResourcePath);
+            }
+
+            return _instance;
+        }
+    }
 }
