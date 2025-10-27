fe8d9bc6c497e277356943a7a621332ac67cd34e
Matthew Dadoun <33492474+matthewdadoun@users.noreply.github.com>
2025-10-26 20:17:59 -0400

    Rename DestructiblePlayer to PlayerStatus + move death callback to public function

---
+++ Assets/Scripts/Types/Objects/PlayerStatus.cs
@@ -0,0 +1,42 @@
+using UnityEngine.SceneManagement;
+
+public class PlayerStatus : DestructibleObject
+{
+    public override void ReactTo(ElementType other)
+    {
+        if (IsOpposingElement(other))
+        {
+            KillPlayer();
+        }
+
+        base.ReactTo(other);
+        return;
+    }
+
+    public void KillPlayer(bool bDestroyPlayer = false)
+    {
+        var fadeInstance = GameManager.Instance.FadeControllerInstance;
+
+        // Retrieve game manager instance / set up binding
+        fadeInstance.OnFadeComplete += OnDeathFadeComplete;
+
+        // Fade out to black
+        fadeInstance.FadeOutToBlack(3f);
+
+        // OnDestroyFadeComplete
+        void OnDeathFadeComplete()
+        {
+            // Unbind event
+            GameManager.Instance.FadeControllerInstance.OnFadeComplete -= OnDeathFadeComplete;
+
+            // Reload the active scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        // If we want to destroy the player, do so
+        if (bDestroyPlayer)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
