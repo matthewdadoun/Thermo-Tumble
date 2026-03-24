c5516581ac00d8d1c63ad4e50ac34d8c82f8e8b0
Matthew Dadoun <33492474+matthewdadoun@users.noreply.github.com>
2026-03-24 19:52:02 -0400

    Main menu - support controller

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,8 +1,17 @@
+using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject initialFocus;
+    
+    private void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(initialFocus);
+    }
+
     public void OpenLevel(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
